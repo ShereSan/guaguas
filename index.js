c83a454e7fee7f86c87ab128421d113e9d35d01c
Scherezade Santana <sherezade1988@gmail.com>
2024-02-29 11:52:56 +0000

    GENERAL - Sequelize install - Connect DB

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 require('dotenv').config()
 const morgan = require('morgan') // informacion de las peticiones que llegan al server
 const express = require('express') // requerimos express
-
+const sequelize = require('./db')
 
 const api = express()
+
+const dbCheck = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log('Connected to Guaguas DB')
+
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
 api.use(morgan('dev'))
 api.use(express.json())
-api.get('/', (req,res) => res.send('Connected to Guaguas API'))
+api.get('/', (req, res) => res.send('Connected to Guaguas API'))
 
-api.listen(process.env.PORT, (err) => {
+api.listen(process.env.PORT, async (err) => {
     if (err) throw new Error('Cannot star API')
-
     console.log('*'.repeat(50))
     console.log(`Guaguas API Running on port ${process.env.PORT}`)
+    await dbCheck()
     console.log('*'.repeat(50))
+
 })
