52695c16a614588d8274efc187932cfab0890e99
Scherezade Santana <sherezade1988@gmail.com>
2024-02-29 11:33:38 +0000

    GENERAL - Express setup

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
+const morgan = require('morgan') // informacion de las peticiones que llegan al server
 const express = require('express') // requerimos express
 
 
 const api = express()
-
+api.use(morgan('dev'))
+api.use(express.json())
+api.get('/', (req,res) => res.send('Connected to Guaguas API'))
 
 api.listen(process.env.PORT, (err) => {
     if (err) throw new Error('Cannot star API')
